8d80c57c9e9367c4e7adb7325f756b848e6a0121
Paul Logston <paul.logston@gmail.com>
2021-06-12 15:20:27 -0700

    Add branch checkout

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
+	"regexp"
 )
+
+var headReFormat = regexp.MustCompile(`ref: refs/heads/(?P<branch>.*)`)
 
 func main() {
 	wd, err := os.Getwd()
@@ -22,11 +26,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(file)
+	data := make([]byte, 1024)
+	_, err = file.Read(data)
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("Unable to read current branch.")
+		os.Exit(1)
+	}
 
-	// list branches
+	currentBranch, err := parseRefForBranch(string(data))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Current branch", currentBranch)
+
+	files, err := os.ReadDir(path.Join(wd, ".git", "refs", "heads"))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// checkout each branch
+	for _, file := range files {
+		branch := file.Name()
+		fmt.Println(branch)
+		if err := checkoutBranch(branch); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if err := checkoutBranch(currentBranch); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// rebase on master
 
@@ -34,6 +68,26 @@ func main() {
 	// If no issues,
 	//    If nothing there, great delete branch
 	//    If soemthing there, leave it be.
+}
 
-	fmt.Println("vim-go")
+func parseRefForBranch(ref string) (string, error) {
+	match := headReFormat.FindStringSubmatch(ref)
+	result := make(map[string]string)
+	for i, name := range headReFormat.SubexpNames() {
+		if i != 0 && name != "" {
+			result[name] = match[i]
+		}
+	}
+
+	branch, ok := result["branch"]
+	if !ok {
+		return "", errors.New("Unable to parse branch name.")
+	}
+
+	return branch, nil
+}
+
+func checkoutBranch(branch string) error {
+	cmd := exec.Command("git", "checkout", branch)
+	return cmd.Run()
 }
