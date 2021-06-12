3267d38d7a3922f6914c5dfa8150b9ec4098fce4
Paul Logston <paul.logston@gmail.com>
2021-06-12 15:39:38 -0700

    Test comment

--- main.go
+++ main.go
@@ -100,6 +100,8 @@ func checkoutBranch(branch string) error {
 
 func rebaseBranch(baseBranch string) error {
 	cmd := exec.Command("git", "rebase", baseBranch)
+
+	// This seems silly
 	stdOE, err := cmd.CombinedOutput()
 	fmt.Println(string(stdOE))
 	if err != nil {
